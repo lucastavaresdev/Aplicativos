f8dfb12dcbab0473d0adebe23a937fef1727172b
Lucas Tavares <lucastavares@ioasys.com>
2020-08-31 18:58:44 -0300

    .

---
+++ freshfood/app/src/main/java/com/lucastomiati/freshfoodapp/SetupToolBar.kt
@@ -0,0 +1,4 @@
+package com.lucastomiati.freshfoodapp
+
+class SetupToolBar {
+}
---
+++ freshfood/app/src/main/java/com/lucastomiati/freshfoodapp/fraguiments/Home4Fragment.kt
@@ -0,0 +1,24 @@
+package com.lucastomiati.freshfoodapp.fraguiments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.lucastomiati.freshfoodapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Home4Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home4, container, false)
+    }
+
+}
